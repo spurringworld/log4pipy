71a6d8117f1cb59b3ccced860091f80082239877
spurringworld <lqhome@126.com>
2023-03-23 10:07:35 +0800

    delete old files

--- postgresql/main.go
+++ postgresql/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-pg/pg/v10"
@@ -58,8 +59,10 @@ func main() {
 	// save logs func
 	///////////////////////
 	r.POST("/logs", func(c *gin.Context) {
-		fmt.Println("")
-		fmt.Println("------------")
+		tableName := c.Query("table")
+		if tableName == "" {
+			tableName = "trafficlogs"
+		}
 		body, err := ioutil.ReadAll(c.Request.Body)
 		if err != nil {
 			fmt.Println("Invalid request body")
@@ -69,35 +72,71 @@ func main() {
 		}
 		// fmt.Println("【body】:  " + string(body))
 		messages := strings.Split(string(body), "\n")
+		baseSql := fmt.Sprintf(`
+		  insert into %s (req_size, res_size, req_time, res_time, end_time, 
+			remote_addr, local_addr, remote_port, local_port, req_path, req_method, 
+			req_protocol, req_headers, res_status, service_name, pod_name, mesh_name,
+			cluster_name, bond_type, message) values 
+		`, tableName)
 		var logList []Trafficlogs
+		idx := 0
 		for _, v := range messages {
 			var msg Message
 			json.Unmarshal([]byte(v), &msg)
-			pipyLog := Trafficlogs{
-				Message:     v,
-				ReqSize:     msg.ReqSize,
-				ResSize:     msg.ResSize,
-				ReqTime:     msg.ReqTime,
-				ResTime:     msg.ResTime,
-				EndTime:     msg.EndTime,
-				RemoteAddr:  msg.RemoteAddr,
-				LocalAddr:   msg.LocalAddr,
-				RemotePort:  msg.RemotePort,
-				LocalPort:   msg.LocalPort,
-				ReqPath:     msg.Req.Path,
-				ReqMethod:   msg.Req.Method,
-				ReqProtocol: msg.Req.Protocol,
-				ReqHeaders:  msg.Req.Headers,
-				ResStatus:   msg.Res.Status,
-				ServiceName: msg.Service.Name,
-				PodName:     msg.Pod.Name,
-				MeshName:    msg.MeshName,
-				ClusterName: msg.ClusterName,
-				BondType:    msg.Type,
+			if idx > 0 {
+				baseSql += ", "
 			}
-			logList = append(logList, pipyLog)
+			valueSql := msg.ReqSize + ","
+			valueSql += msg.ResSize + ","
+			valueSql += msg.ReqTime + ","
+			valueSql += msg.ResTime + ","
+			valueSql += msg.EndTime + ","
+
+			valueSql += msg.RemoteAddr + ","
+			valueSql += msg.LocalAddr + ","
+			valueSql += msg.RemotePort + ","
+			valueSql += msg.LocalPort + ","
+
+			valueSql += msg.Req.Path + ","
+			valueSql += msg.Req.Method + ","
+			valueSql += msg.Req.Protocol + ","
+			valueSql += msg.Req.Headers + ","
+			valueSql += msg.msg.Res.Status + ","
+			valueSql += msg.Service.Name + ","
+			valueSql += msg.Pod.Name + ","
+			valueSql += msg.MeshName + ","
+			valueSql += msg.ClusterName + ","
+			valueSql += msg.Type + ","
+			valueSql += v
+
+			// pipyLog := Trafficlogs{
+			// 	Message:     v,
+			// 	ReqSize:     msg.ReqSize,
+			// 	ResSize:     msg.ResSize,
+			// 	ReqTime:     msg.ReqTime,
+			// 	ResTime:     msg.ResTime,
+			// 	EndTime:     msg.EndTime,
+			// 	RemoteAddr:  msg.RemoteAddr,
+			// 	LocalAddr:   msg.LocalAddr,
+			// 	RemotePort:  msg.RemotePort,
+			// 	LocalPort:   msg.LocalPort,
+			// 	ReqPath:     msg.Req.Path,
+			// 	ReqMethod:   msg.Req.Method,
+			// 	ReqProtocol: msg.Req.Protocol,
+			// 	ReqHeaders:  msg.Req.Headers,
+			// 	ResStatus:   msg.Res.Status,
+			// 	ServiceName: msg.Service.Name,
+			// 	PodName:     msg.Pod.Name,
+			// 	MeshName:    msg.MeshName,
+			// 	ClusterName: msg.ClusterName,
+			// 	BondType:    msg.Type,
+			// }
+			// logList = append(logList, pipyLog)
+
+			baseSql += fmt.Sprintf("(%s)", valueSql)
+			idx++
 		}
-		result, err := conn.Model(&logList).Insert()
+		result, err := conn.Model(&logList).TableExpr(tableName).Insert()
 		if err != nil {
 			fmt.Println("batch insert rows error: ", err)
 			c.JSON(500, gin.H{
@@ -109,6 +148,69 @@ func main() {
 				"message": "success.",
 			})
 		}
+	})
+
+	///////////////////////
+	// to ping func
+	///////////////////////
+	r.GET("/logs", func(c *gin.Context) {
+		baseSql := "SELECT NOW() AS nt"
+		// get total
+		var result struct {
+			Nt time.Time
+		}
+		_, err1 := conn.QueryOne(&result, baseSql)
+		if err1 != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err1.Error()})
+			return
+		}
+		c.JSON(200, gin.H{
+			"statusText": "success",
+			"data":       result.Nt,
+		})
+	})
+
+	///////////////////////
+	// query logs func(for test)
+	///////////////////////
+	r.GET("/querylogs", func(c *gin.Context) {
+		tableName := c.Query("table")
+		if tableName == "" {
+			tableName = "trafficlogs"
+		}
+		baseSql := fmt.Sprintf(`
+		SELECT service_name, pod_name, req_path, req_method, req_protocol,
+			res_time, req_time, res_status, res_size,
+			remote_addr, remote_port, local_addr, local_port,
+			created_at, req_headers, message
+		FROM %s
+		WHERE  bond_type != 'outbotrafficlogsund'
+		`, tableName)
+		// get total
+		var countResult struct {
+			Total uint64
+		}
+		countSql := fmt.Sprintf("SELECT count(1) AS total FROM (%s) abc", baseSql)
+		// fmt.Println(countSql)
+		_, err1 := conn.QueryOne(&countResult, countSql)
+		if err1 != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err1.Error()})
+			return
+		}
+		// get data
+		querySql := baseSql + " LIMIT 1 OFFSET 0"
+		// fmt.Println(querySql)
+		var result []LogVO
+		_, err2 := conn.Query(&result, querySql)
+		if err2 != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err2.Error()})
+			return
+		}
+		c.JSON(200, gin.H{
+			"statusText": "success",
+			"data":       result,
+			"total":      countResult.Total,
+		})
 	})
 
 	///////////////////////
